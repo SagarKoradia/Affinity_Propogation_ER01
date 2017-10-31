eed54c6784499c9067efa4bfcdf6f6f6109d4f49
Sagar Koradia <31327738+SagarKoradia@users.noreply.github.com>
2017-11-01 06:32:25 +1100

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,8 @@ from sklearn.cluster import AffinityPropagation
 fn = r'C:\Users\DELL I5558\Desktop\Python\NSW-ER01.csv'
 my_data = genfromtxt(fn, delimiter=',')
 
-af = AffinityPropagation(damping=0.6, max_iter=2000, convergence_iter=1000, copy=True, preference=None, affinity='euclidean', verbose=False).fit(my_data)
+af = AffinityPropagation(damping=0.6, max_iter=2000, convergence_iter=1000, copy=True, preference=None, affinity='euclidean', 
+                         verbose=False).fit(my_data)
 cluster_centers_indices = af.cluster_centers_indices_
 labels = af.labels_
 
